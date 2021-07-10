2a3cf4c461f2d880163f1fd9e929d81307b760cf
Joseph Lai <jlai24142@gmail.com>
2021-07-10 17:48:34 -0400

    Adding function for getting the current working directory

---
+++ modify/path.go
@@ -0,0 +1,13 @@
+// Get filepaths used throughout this program.
+
+package modify
+
+import "os"
+
+// Get the current working directory.
+func GetCWD() string {
+	cwd, err := os.Getwd()
+	CheckError("Could not get the current working directory", err)
+
+	return cwd
+}
