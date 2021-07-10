ca51dc32de8fd98ab95073fe7258eacd04145d95
Joseph Lai <jlai24142@gmail.com>
2021-07-10 17:46:57 -0400

    Adding colors

--- utils/colors.go
+++ utils/colors.go
@@ -6,6 +6,12 @@ import (
 	"github.com/fatih/color"
 )
 
-var blue = color.New(color.FgBlue).Add(color.Bold)
-var green = color.New(color.FgGreen).Add(color.Bold)
-var white = color.New(color.FgWhite).Add(color.Bold)
+var Blue = color.New(color.FgBlue).Add(color.Bold)
+var Green = color.New(color.FgGreen).Add(color.Bold)
+var Red = color.New(color.FgRed).Add((color.Bold))
+var White = color.New(color.FgWhite).Add(color.Bold)
+var Yellow = color.New(color.FgYellow).Add(color.Bold)
+
+var BlueSprint = color.New(color.FgBlue).Add(color.Bold).SprintFunc()
+var WhiteSprint = color.New(color.FgWhite).Add(color.Bold).SprintFunc()
+var YellowSprint = color.New(color.FgYellow).Add(color.Bold).SprintFunc()
