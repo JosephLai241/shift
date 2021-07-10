8c68a3bfdec40c324b9219ef6c850abc599948d2
Joseph Lai <jlai24142@gmail.com>
2021-07-10 14:25:05 -0400

    Adding colors

---
+++ utils/colors.go
@@ -0,0 +1,11 @@
+// Defining terminal text color and style.
+
+package utils
+
+import (
+	"github.com/fatih/color"
+)
+
+var blue = color.New(color.FgBlue).Add(color.Bold)
+var green = color.New(color.FgGreen).Add(color.Bold)
+var white = color.New(color.FgWhite).Add(color.Bold)
