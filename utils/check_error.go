0ddcf489573a5c6470bd7d9e8295fed1ff95ba51
Joseph Lai <jlai24142@gmail.com>
2021-07-12 00:12:48 -0400

    Updating colors

--- utils/check_error.go
+++ utils/check_error.go
@@ -7,7 +7,7 @@ import "log"
 // Check if there is an error. Panic if an error is not `nil`.
 func CheckError(message string, err error) {
 	if err != nil {
-		log.Fatal(message, err)
+		log.Fatal(BoldRed.Sprintf(message), err)
 		panic(err)
 	}
 }
