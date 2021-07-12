ce4858e8b170e2022d1bc71183f17403c25bd481
Joseph Lai <jlai24142@gmail.com>
2021-07-12 00:11:44 -0400

    Updating color names. Removing unused colors

--- utils/colors.go
+++ utils/colors.go
@@ -4,12 +4,8 @@ package utils
 
 import "github.com/fatih/color"
 
-var Blue = color.New(color.FgBlue).Add(color.Bold)
-var Green = color.New(color.FgGreen).Add(color.Bold)
-var Red = color.New(color.FgRed).Add((color.Bold))
-var White = color.New(color.FgWhite).Add(color.Bold)
-var Yellow = color.New(color.FgYellow).Add(color.Bold)
-
-var BlueSprint = color.New(color.FgBlue).Add(color.Bold).SprintFunc()
-var WhiteSprint = color.New(color.FgWhite).Add(color.Bold).SprintFunc()
-var YellowSprint = color.New(color.FgYellow).Add(color.Bold).SprintFunc()
+var BoldBlue = color.New(color.FgBlue).Add(color.Bold)
+var BoldGreen = color.New(color.FgGreen).Add(color.Bold)
+var BoldRed = color.New(color.FgRed).Add((color.Bold))
+var BoldWhite = color.New(color.FgWhite).Add(color.Bold)
+var BoldYellow = color.New(color.FgYellow).Add(color.Bold)
