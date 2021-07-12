09e9c1c21254d41b5208c26c8888305f8cfc6275
Joseph Lai <jlai24142@gmail.com>
2021-07-12 16:20:54 -0400

    Importing ASCII art

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,23 +1,11 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+//
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/JosephLai241/shift/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -25,14 +13,16 @@ import (
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Long: `
+   _     _     _       
+ _| |___| |___| |_ ___ 
+| . | -_| | -_|  _| -_|
+|___|___|_|___|_| |___|
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Use this command to delete a recorded shift. You can
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		fmt.Println(utils.DeleteArt)
 	},
 }
 
