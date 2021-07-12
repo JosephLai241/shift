9bb5762dbe81e932307863c06ab1f706fec195f3
Joseph Lai <jlai24142@gmail.com>
2021-07-12 16:20:28 -0400

    Adding single source of ASCII art

---
+++ utils/titles.go
@@ -0,0 +1,52 @@
+// Defining ASCII art used throughout this project.
+
+package utils
+
+var VersionArt string = BoldWhite.Sprint(`
+     _   _ ___ _ 
+ ___| |_|_|  _| |_
+|_ -|   | |  _|  _|
+|___|_|_|_|_| |_|   v1.0.0
+`)
+
+var InArt string = BoldWhite.Sprint(`
+ _
+|_|___ 
+| |   |
+|_|_|_|
+`)
+
+var OutArt string = BoldWhite.Sprint(`
+         _ 
+ ___ _ _| |_
+| . | | |  _|
+|___|___|_|
+`)
+
+var StatusArt string = BoldWhite.Sprint(`
+     _       _
+ ___| |_ ___| |_ _ _ ___ 
+|_ -|  _| .'|  _| | |_ -|
+|___|_| |__,|_| |___|___|
+`)
+
+var AmendArt string = BoldWhite.Sprint(`
+                     _
+ ___ _____ ___ ___ _| |
+| .'|     | -_|   | . |
+|__,|_|_|_|___|_|_|___|
+`)
+
+var ListArt string = BoldWhite.Sprint(`
+ _ _     _
+| |_|___| |_
+| | |_ -|  _|
+|_|_|___|_|
+`)
+
+var DeleteArt string = BoldWhite.Sprint(`
+   _     _     _
+ _| |___| |___| |_ ___
+| . | -_| | -_|  _| -_|
+|___|___|_|___|_| |___|
+`)
