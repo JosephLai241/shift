c984243047f366e4130bb8148a7bbfd3428091ca
Joseph Lai <jlai24142@gmail.com>
2021-07-14 03:10:36 -0400

    Moving checks for the '-d' flag to a separate file - can be reused

---
+++ cmd/check_flag.go
@@ -0,0 +1,89 @@
+// Storing functions that may be reused to check flags.
+
+package cmd
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Check whether the input date is valid.
+func checkDate(splitDate []string) (bool, string) {
+	validMonths := map[string]struct{ days int }{
+		"01": {days: 31},
+		"02": {days: 28},
+		"03": {days: 31},
+		"04": {days: 30},
+		"05": {days: 31},
+		"06": {days: 30},
+		"07": {days: 31},
+		"08": {days: 30},
+		"09": {days: 31},
+		"10": {days: 30},
+		"11": {days: 31},
+		"12": {days: 30},
+	}
+
+	if len(splitDate[0]) > 2 || len(splitDate[1]) > 2 || len(splitDate[2]) > 4 {
+		return false, "Invalid date. Accepted formats: MM-DD-YYYY, MM/DD/YYYY"
+	}
+
+	inputDay, dayErr := strconv.Atoi(splitDate[1])
+	if dayErr != nil {
+		return false, "Invalid day."
+	}
+	inputYear, yearErr := strconv.Atoi(splitDate[2])
+	if yearErr != nil {
+		return false, "Invalid year."
+	}
+	currentYear, _ := strconv.Atoi(time.Now().Format("2006"))
+
+	if dayStruct, ok := validMonths[splitDate[0]]; !ok {
+		return false, "Invalid month."
+	} else if ok && inputDay > dayStruct.days {
+		return false, "Invalid day."
+	} else if ok && inputYear < currentYear {
+		return false, "Invalid year."
+	}
+
+	return true, "valid"
+}
+
+// Check whether the input day of the week is valid.
+func checkDays(searchParam string) (bool, string) {
+	validDays := map[string]struct{}{
+		"monday":    {},
+		"tuesday":   {},
+		"wednesday": {},
+		"thursday":  {},
+		"friday":    {},
+		"saturday":  {},
+		"sunday":    {},
+	}
+
+	if _, ok := validDays[strings.ToLower(searchParam)]; !ok {
+		return false, "Invalid day of the week."
+	}
+
+	return true, "valid"
+}
+
+// Call checkDate() and checkDays() in one helper function.
+func checkDFlag(searchParam string) (bool, string, string) {
+	var newUserInput string
+	if strings.Contains(searchParam, "-") {
+		splitDate := strings.Split(searchParam, "-")
+		if isValid, err := checkDate(splitDate); !isValid {
+			return false, searchParam, err
+		}
+		newUserInput = searchParam
+	} else {
+		if isValid, err := checkDays(searchParam); !isValid {
+			return false, searchParam, err
+		}
+		newUserInput = strings.Title(searchParam)
+	}
+
+	return true, newUserInput, "valid"
+}
