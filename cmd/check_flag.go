4f77ddbcf966968a2b1adee06e1f27401d6c9ce1
Joseph Lai <jlai24142@gmail.com>
2021-07-15 02:11:27 -0400

    Adding a function to check wiether the input month is valid

--- cmd/check_flag.go
+++ cmd/check_flag.go
@@ -87,3 +87,28 @@ func checkDFlag(searchParam string) (bool, string, string) {
 
 	return true, newUserInput, "valid"
 }
+
+// Check whether the month is valid.
+func checkMonth(searchMonth string) (bool, string, string) {
+	validMonths := map[string]string{
+		"January":   "01",
+		"February":  "02",
+		"March":     "03",
+		"April":     "04",
+		"May":       "05",
+		"June":      "06",
+		"July":      "07",
+		"August":    "08",
+		"September": "09",
+		"October":   "10",
+		"November":  "11",
+		"December":  "12",
+	}
+
+	monthNum, ok := validMonths[searchMonth]
+	if !ok {
+		return false, "", "Invalid month."
+	}
+
+	return true, monthNum, "valid"
+}
