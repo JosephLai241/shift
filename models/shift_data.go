a15fa727c14aeaa9b70758cd3db9fbbf17cabf88
Joseph Lai <jlai24142@gmail.com>
2021-07-15 19:42:22 -0400

    Adding methods to find matching shifts and amend the target shift

--- models/shift_data.go
+++ models/shift_data.go
@@ -5,6 +5,8 @@ package models
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/JosephLai241/shift/modify"
@@ -20,6 +22,7 @@ type ShiftData struct {
 }
 
 // Timesheet functions.
+// --------------------
 
 // Write clock-in data to the timesheet. Add the column titles if the timesheet
 // was just created.
@@ -47,16 +50,20 @@ func (shiftData ShiftData) recordIn(overwriteFile *os.File, rows [][]string) {
 		"",
 	}
 	rows = append(rows, newRow)
-	modify.WriteToTimesheet(overwriteFile, rows)
 
+	modify.WriteToTimesheet(overwriteFile, rows)
 }
 
 // Write clock-out data to the timesheet.
 func (shiftData ShiftData) recordOut(overwriteFile *os.File, rows [][]string) {
 	for i, row := range rows {
 		if i == len(rows)-1 {
-			startTime, _ := time.Parse("15:04:05", row[2])
-			endTime, _ := time.Parse("15:04:05", shiftData.Time)
+			startString := fmt.Sprintf("%s %s %s", row[0], row[1], row[2])
+			endString := fmt.Sprintf("%s %s", time.Now().Format("01-02-2006 Monday"), shiftData.Time)
+
+			startTime, _ := time.Parse("01-02-2006 Monday 15:04:05", startString)
+			endTime, _ := time.Parse("01-02-2006 Monday 15:04:05", endString)
+
 			duration := endTime.Sub(startTime)
 
 			row[4] = shiftData.Time
@@ -84,4 +91,46 @@ func (shiftData *ShiftData) RecordShift() {
 	}
 }
 
+// Find and return matches based on the day or date.
+func FindMatches(dayOrDate string, rows [][]string) ([]int, [][]string) {
+	var targetIndex int
+	switch contains := strings.Contains(dayOrDate, "-"); contains {
+	case true:
+		targetIndex = 0
+	case false:
+		targetIndex = 1
+	}
+
+	var matches [][]string
+	var rowNums []int
+	for i, row := range rows {
+		if row[targetIndex] == dayOrDate {
+			matchRow := append([]string{strconv.Itoa(i)}, row...)
+			matches = append(matches, matchRow)
+			rowNums = append(rowNums, i)
+		}
+	}
+
+	if len(matches) < 1 {
+		return nil, nil
+	}
+
+	return rowNums, matches
+}
+
+// Amend a clock-in or clock-out message.
+func AmendMessage(amendRow []string, newMessage string, target string) {
+	var targetIndex int
+
+	switch {
+	case target == "in":
+		targetIndex = 3
+	case target == "out":
+		targetIndex = 5
+	}
+
+	amendRow[targetIndex] = newMessage
+}
+
 // SQLite functions.
+// -----------------
