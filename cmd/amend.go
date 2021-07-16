ea9d3f7bd0a269d3a30eaf633778a940bb2e5e9e
Joseph Lai <jlai24142@gmail.com>
2021-07-16 04:03:53 -0400

    Adding functions to the amend command

--- cmd/amend.go
+++ cmd/amend.go
@@ -1,16 +1,18 @@
-//
+// Defining the `amend` command.
 
 package cmd
 
 import (
+	"errors"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/JosephLai241/shift/models"
 	"github.com/JosephLai241/shift/modify"
 	"github.com/JosephLai241/shift/utils"
+	"github.com/JosephLai241/shift/views"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -18,7 +20,7 @@ import (
 // amendCmd represents the amend command
 var amendCmd = &cobra.Command{
 	Use:   "amend (in|out)",
-	Short: "A brief description of your command",
+	Short: "Amend a shift's clock-in or clock-out message",
 	Long: `
                      _ 
  ___ _____ ___ ___ _| |
@@ -30,10 +32,19 @@ Use this command to amend a recorded shift's clock-in or clock-out message.
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(utils.AmendArt)
 
-		userInput := checkInput(args, cmd)
+		checkArgs(args)
+
+		dayOrDate, _ := cmd.Flags().GetString("dayordate")
+		month, _ := cmd.Flags().GetString("month")
+		year, _ := cmd.Flags().GetString("year")
+
+		amendDayOrDate(&dayOrDate)
+		amendMonth(&dayOrDate, month)
+		amendYear(&dayOrDate, year)
+
 		switch storageType := viper.GetString("storage-type"); storageType {
 		case "timesheet":
-			amendTimesheet(userInput)
+			amendTimesheet(args, dayOrDate, month, year)
 		case "database":
 			fmt.Println("DATABASE SELECTED")
 		}
@@ -48,125 +59,105 @@ func init() {
 		time.Now().Format("01-02-2006"),
 		"Narrow your search by the day of the week or by a date",
 	)
-}
-
-// Check whether the input date is valid.
-func checkDate(splitDate []string) (bool, string) {
-	validMonths := map[string]struct{ days int }{
-		"01": {days: 31},
-		"02": {days: 28},
-		"03": {days: 31},
-		"04": {days: 30},
-		"05": {days: 31},
-		"06": {days: 30},
-		"07": {days: 31},
-		"08": {days: 30},
-		"09": {days: 31},
-		"10": {days: 30},
-		"11": {days: 31},
-		"12": {days: 30},
-	}
-
-	if len(splitDate[0]) > 2 || len(splitDate[1]) > 2 || len(splitDate[2]) > 4 {
-		return false, "Invalid date."
-	}
-
-	inputDay, dayErr := strconv.Atoi(splitDate[1])
-	if dayErr != nil {
-		return false, "Invalid day."
-	}
-	inputYear, yearErr := strconv.Atoi(splitDate[2])
-	if yearErr != nil {
-		return false, "Invalid year."
-	}
-	currentYear, _ := strconv.Atoi(time.Now().Format("2006"))
-
-	if dayStruct, ok := validMonths[splitDate[0]]; !ok {
-		return false, "Invalid month."
-	} else if ok && inputDay > dayStruct.days {
-		return false, "Invalid day."
-	} else if ok && inputYear < currentYear {
-		return false, "Invalid year."
-	}
-
-	return true, "valid"
-}
-
-// Check whether the input day of the week is valid.
-func checkDays(userInput string) (bool, string) {
-	validDays := map[string]struct{}{
-		"monday":    {},
-		"tuesday":   {},
-		"wednesday": {},
-		"thursday":  {},
-		"friday":    {},
-		"saturday":  {},
-		"sunday":    {},
-	}
-
-	if _, ok := validDays[strings.ToLower(userInput)]; !ok {
-		return false, "Invalid day of the week."
-	}
-
-	return true, "valid"
-}
-
-// Call checkDate() and checkDays() in one helper function.
-func checkDFlag(userInput string) (bool, string) {
-	if strings.Contains(userInput, "/") || strings.Contains(userInput, "-") {
-		var splitDate []string
-		switch true {
-		case strings.Contains(userInput, "/"):
-			splitDate = strings.Split(userInput, "/")
-		case strings.Contains(userInput, "-"):
-			splitDate = strings.Split(userInput, "-")
-		}
-
-		if isValid, err := checkDate(splitDate); !isValid {
-			return false, err
-		}
-	} else {
-		if isValid, err := checkDays(userInput); !isValid {
-			return false, err
-		}
-	}
-
-	return true, "valid"
+	amendCmd.Flags().StringP(
+		"month", "m",
+		time.Now().Format("January"),
+		"List records in a specific month",
+	)
+	amendCmd.Flags().StringP(
+		"year", "y",
+		time.Now().Format("2006"),
+		"List records in a specific year",
+	)
 }
 
 // Check all input for the `amend` command.
-func checkInput(args []string, cmd *cobra.Command) string {
+func checkArgs(args []string) {
 	if len(args) < 1 {
-		utils.BoldRed.Println("`amend` requires in or out.")
-		fmt.Println("")
-		os.Exit(1)
+		utils.CheckError("Command error", errors.New("`amend` requires in or out"))
 	} else if len(args) < 2 {
-		utils.BoldRed.Println("`amend` requires a new message.")
-		fmt.Println("")
-		os.Exit(1)
+		utils.CheckError("Command error", errors.New("`amend` requires a new message"))
 	} else {
 		utils.BoldBlue.Printf("New message: %s\n", args[len(args)-1])
 	}
+}
 
-	userInput, _ := cmd.Flags().GetString("dayordate")
-	if len(userInput) > 1 {
-		if isValid, response := checkDFlag(userInput); !isValid && response != "valid" {
-			utils.BoldRed.Printf("\n%s\n", response)
-			fmt.Println("")
-			os.Exit(1)
-		}
+// Check if the selection is valid.
+func checkSelection(rowNums []int) int {
+	validOptions := make(map[int]struct{})
+	for i := range rowNums {
+		validOptions[rowNums[i]] = struct{}{}
 	}
 
-	return userInput
+	var input string
+	for {
+		fmt.Printf("\nSelect a shift to modify %+v: ", rowNums)
+		fmt.Scanln(&input)
+
+		intSelection, _ := strconv.Atoi(input)
+		if _, ok := validOptions[intSelection]; !ok {
+			utils.BoldRed.Print("\nInvalid option. Try again.\n")
+		} else {
+			return intSelection
+		}
+	}
+}
+
+// Amend the data in the selected shift and display the updated data.
+func displayUpdate(args []string, rows [][]string, rowNums []int) ([]string, int) {
+	intSelection := checkSelection(rowNums)
+	amendRow := rows[intSelection]
+	models.AmendMessage(amendRow, args[1], args[0])
+
+	fmt.Println("")
+	utils.BoldWhite.Println("CHANGES")
+	views.Display([][]string{amendRow})
+
+	return amendRow, intSelection
+}
+
+// Amend the target shift.
+func amendShift(amendRow []string, intSelection int, month string, rows [][]string, year string) {
+	rows[intSelection] = amendRow
+
+	overwriteTimesheet, err := getTimesheetByDFlags(month, true, year)
+	if err != nil {
+		utils.CheckError("Unable to open the timesheet to overwrite", err)
+	}
+
+	modify.WriteToTimesheet(overwriteTimesheet, rows)
 }
 
 // Amend a shift.
-func amendTimesheet(userInput string) {
-	timesheetDirectory := modify.InitializeDirectories()
-	timesheetPath := modify.GetTimesheetPath(timesheetDirectory)
-	timesheet, err := os.OpenFile(timesheetPath, os.O_RDWR, 0755)
-	utils.CheckError("Unable to open the timesheet", err)
+func amendTimesheet(args []string, dayOrDate string, month string, year string) {
+	timesheet, err := getTimesheetByDFlags(month, false, year)
+	if err != nil {
+		utils.CheckError(
+			fmt.Sprintf("An error occurred when listing shifts recorded in %s %s", strings.Title(month), year),
+			errors.New("no shifts were recorded"),
+		)
+	}
 
 	rows := modify.ReadTimesheet(timesheet)
-	fmt.Println(rows)
+	fmt.Println("")
+
+	switch rowNums, matches := models.FindMatches(dayOrDate, rows); len(rowNums) {
+	case 0:
+		utils.CheckError("Error", fmt.Errorf("no shifts were found on %s", dayOrDate))
+	default:
+		utils.BoldWhite.Println("MATCHES")
+		views.DisplayOptions(matches)
+
+		amendRow, intSelection := displayUpdate(args, rows, rowNums)
+
+		switch confirmation := utils.ConfirmInput("revision"); confirmation {
+		case "y":
+			amendShift(amendRow, intSelection, month, rows, year)
+			utils.BoldGreen.Printf("\nSuccessfully amended clock-%s message on %s.\n", args[0], dayOrDate)
+		case "n":
+			utils.BoldYellow.Printf("\nABORTING.\n")
+		}
+	}
+
+	fmt.Println("")
 }
