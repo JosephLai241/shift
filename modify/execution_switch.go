01d7a7a7342bd85b8007ec7514031950ba6e510c
Joseph Lai <jlai24142@gmail.com>
2021-07-16 20:26:03 -0400

    Updating function comment

--- modify/execution_switch.go
+++ modify/execution_switch.go
@@ -5,7 +5,7 @@ package modify
 import "github.com/spf13/viper"
 
 // Main CRUD switch execute the timesheet or database function depending on the
-// `storage-type` value.
+// `storage-type` value set in `.shiftconfig.yml`.
 func CRUD(timesheetFn func(), databaseFn func()) {
 	switch storageType := viper.GetString("storage-type"); storageType {
 	case "timesheet":
