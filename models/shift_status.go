17d35dcb67e48d241abb0b2eb234af1f02b4b5d6
Joseph Lai <jlai24142@gmail.com>
2021-07-17 15:01:21 -0400

    Refactoring and restructuring function order

--- models/shift_status.go
+++ models/shift_status.go
@@ -18,83 +18,6 @@ import (
 var cwd = utils.GetCWD()
 var DotfileName = fmt.Sprintf("%s/.%s", cwd, "shiftstatus")
 
-// Check the current shift status.
-// Return `false` if `.shiftstatus` dotfile exists or `STATUS` is currently `ACTIVE`.
-func CheckStatus() (bool, error) {
-	if dotfile, err := os.Open(DotfileName); err != nil {
-		return false, errors.New("'.shiftstatus' does not exist")
-	} else {
-		scanner := bufio.NewScanner(dotfile)
-		for scanner.Scan() {
-			splitString := strings.Split(scanner.Text(), "=")
-			if splitString[0] == "STATUS" && splitString[1] == "ACTIVE" {
-				return true, nil
-			}
-		}
-		return false, nil
-	}
-}
-
-// Check the `storage-type` value in `.shiftstatus`.
-func CheckStorageType() string {
-	var storageType string
-	if dotfile, err := os.Open(DotfileName); err != nil {
-		utils.CheckError(
-			"Error reading '.shiftstatus'",
-			errors.New("'.shiftstatus' does not exist"),
-		)
-	} else {
-		scanner := bufio.NewScanner(dotfile)
-		for scanner.Scan() {
-			splitString := strings.Split(scanner.Text(), "=")
-			if splitString[0] == "storage-type" {
-				storageType = splitString[1]
-			}
-		}
-	}
-
-	return storageType
-}
-
-// Display the current shift status set in `.shiftstatus`.
-func DisplayStatus(displayState bool) {
-	dotfile, err := os.Open(DotfileName)
-	utils.CheckError("Could not open .shiftstatus dotfile", err)
-	defer dotfile.Close()
-
-	printFields := map[string]struct{}{
-		"STATUS":            {},
-		"Clock-in Time":     {},
-		"Clock-in Message":  {},
-		"Clock-out Time":    {},
-		"Clock-out Message": {},
-	}
-
-	scanner := bufio.NewScanner(dotfile)
-	var duration string
-	for scanner.Scan() {
-		splitString := strings.Split(scanner.Text(), "=")
-		if _, ok := printFields[splitString[0]]; ok {
-			if splitString[0] == "STATUS" {
-				if splitString[1] == "ACTIVE" && displayState {
-					utils.BoldGreen.Add(color.Italic).Printf("%s\n\n", splitString[1])
-				}
-			} else if splitString[0] == "Clock-in Time" {
-				fmt.Printf("%s: %s\n", splitString[0], splitString[1])
-				start, _ := time.ParseInLocation("01-02-2006 15:04:05 Monday", splitString[1], time.Now().Location())
-				durationObject := time.Since(start)
-				duration = time.Time{}.Add(durationObject).Format("15:04:05")
-			} else {
-				fmt.Printf("%s: %s\n", splitString[0], splitString[1])
-			}
-		}
-	}
-	if len(duration) > 1 {
-		utils.BoldBlue.Printf("\nDuration: %s\n", duration)
-	}
-	fmt.Println("")
-}
-
 // Format the string that will be written to the `.shiftstatus` dotfile.
 func formatStatus(io string, ss ShiftStatus) string {
 	status := fmt.Sprintf("STATUS=%s\n%s Time=%s\n", ss.Status, io, ss.Time)
@@ -108,13 +31,13 @@ func formatStatus(io string, ss ShiftStatus) string {
 
 // Shift status struct.
 type ShiftStatus struct {
-	Type    string
-	Status  string
-	Time    string
-	Message string
+	Type    string // "IN" or "OUT" (clock-in/out)
+	Status  string // "ACTIVE" or "READY"
+	Time    string // Clock-in or clock-out time
+	Message string // Clock-in or clock-out messsage
 }
 
-// Set the shift status in the `.shiftstatus` dotfile.
+// Set the shift status data in `.shiftstatus`.
 func (ss *ShiftStatus) SetStatus() {
 	dotfile, createErr := os.Create(DotfileName)
 	utils.CheckError("Could not create .shiftstatus dotfile", createErr)
@@ -131,4 +54,80 @@ func (ss *ShiftStatus) SetStatus() {
 	utils.CheckError("Could not write to .shiftstatus dotfile", writeErr)
 
 	dotfile.Close()
+}
+
+// Read the `.shiftstatus` dotfile and convert the data into a map.
+func readDotfile() (map[string]string, error) {
+	dotfileMap := make(map[string]string)
+
+	dotfile, err := os.Open(DotfileName)
+	if err != nil {
+		return nil, errors.New("'.shiftstatus' does not exist")
+	} else {
+		scanner := bufio.NewScanner(dotfile)
+		for scanner.Scan() {
+			splitString := strings.Split(scanner.Text(), "=")
+			dotfileMap[splitString[0]] = splitString[1]
+		}
+	}
+	dotfile.Close()
+
+	return dotfileMap, nil
+}
+
+// Check the current shift status.
+// Return `false` if `.shiftstatus` does not exist or if `STATUS` is not `ACTIVE`.
+func CheckStatus() (bool, error) {
+	dotfileMap, err := readDotfile()
+	if err != nil {
+		return false, err
+	}
+	if status := dotfileMap["STATUS"]; status != "ACTIVE" {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// Get the current `storage-type` value in `.shiftstatus`.
+func GetCurrentStorageType() string {
+	dotfileMap, err := readDotfile()
+	utils.CheckError("Error reading '.shiftstatus'", err)
+
+	return dotfileMap["storage-type"]
+}
+
+// Display the current shift status set in `.shiftstatus`.
+func DisplayStatus(displayState bool) {
+	dotfileMap, err := readDotfile()
+	utils.CheckError("Error reading '.shiftstatus'", err)
+
+	printFields := map[string]struct{}{
+		"Clock-in Time":     {},
+		"Clock-in Message":  {},
+		"Clock-out Time":    {},
+		"Clock-out Message": {},
+	}
+
+	if status := dotfileMap["STATUS"]; status == "ACTIVE" && displayState {
+		utils.BoldGreen.Add(color.Italic).Printf("%s\n\n", status)
+	}
+
+	var duration string
+	for key, value := range dotfileMap {
+		if _, ok := printFields[key]; ok {
+			if key == "Clock-in Time" {
+				fmt.Printf("%s: %s\n", key, value)
+				start, _ := time.ParseInLocation("01-02-2006 15:04:05 Monday", value, time.Now().Location())
+				durationObject := time.Since(start)
+				duration = time.Time{}.Add(durationObject).Format("15:04:05")
+			} else {
+				fmt.Printf("%s: %s\n", key, value)
+			}
+		}
+	}
+	if len(duration) > 1 {
+		utils.BoldBlue.Printf("\nDuration: %s\n", duration)
+	}
+	fmt.Println("")
 }
